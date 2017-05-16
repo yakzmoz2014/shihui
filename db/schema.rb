b3ee24b3d149e711d93aa0e3885386d5d1f247ef
wangdonghui <wangdonghuiibt@gmail.com>
2017-05-16 12:00:09 +0800

    add order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516020117) do
+ActiveRecord::Schema.define(version: 20170516035831) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -33,6 +33,17 @@ ActiveRecord::Schema.define(version: 20170516020117) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "total",            default: 0
+    t.integer  "user_id"
+    t.string   "billing_name"
+    t.string   "billing_address"
+    t.string   "shipping_name"
+    t.string   "shipping_address"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   create_table "users", force: :cascade do |t|
