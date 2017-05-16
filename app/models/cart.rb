eb2c2498ccbc16dcb65b670e206a0aa37a9c5515
wangdonghui <wangdonghuiibt@gmail.com>
2017-05-16 11:13:03 +0800

    implement empty cart method

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -17,4 +17,19 @@ class Cart < ApplicationRecord
     ci.quantity = 1
     ci.save
   end
+
+  def total_price
+    sum = 0
+    cart_items.each do |cart_item|
+      if cart_item.meetup.price.present?
+        sum += cart_item.quantity * cart_item.meetup.price
+      end
+    end
+    sum
+  end
+
+  def clean!
+    cart_items.destroy_all
+  end
+  
 end
