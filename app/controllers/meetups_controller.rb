fa0a9c1a28315ca18143e1b20ced050ca4392d11
wangdonghui <wangdonghuiibt@gmail.com>
2017-05-16 11:27:21 +0800

    cannot add same item to cart

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -9,9 +9,13 @@ class MeetupsController < ApplicationController
 
   def add_to_cart
     @meetup = Meetup.find(params[:id])
-    current_cart.add_meetup_to_cart(@meetup)
-    flash[:notice] = "成功加入购物车"
+    if !current_cart.products.include?(@product)
+      current_cart.add_meetup_to_cart(@meetup)
+      flash[:notice] = "成功加入购物车"
+      flash[:notice] = "你已成功将 #{@meetup.title} 加入购物车"
+    else
+      flash[:warning] = "购物车中已有同类物品"
+    end
     redirect_to :back
-
   end
 end
