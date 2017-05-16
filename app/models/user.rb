90a7bebdcecc0e0126477028bd710eca24ace95a
wangdonghui <wangdonghuiibt@gmail.com>
2017-05-16 12:28:20 +0800

    add order controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+  has_many :orders
+
+  
   def admin?
     is_admin
   end
+
+
 end
